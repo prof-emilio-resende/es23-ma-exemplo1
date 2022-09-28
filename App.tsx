1b3cac049fab074ef644c442d7f647204bf01353
Emilio Resende <emilio.murta@msitbrasil.com>
2022-09-28 20:25:16 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import img from "./assets/icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.text}>
+        <Text>Por favor, selecione uma imagem...</Text>
+      </View>
+      <View style={styles.image}>
+        <Image source={img} style={styles.image} resizeMode="contain" />
+      </View>
+      <View style={styles.actions}>
+        <TouchableOpacity onPress={() => alert('ola!')} style={styles.button}>
+          <Text style={styles.buttonLabel}>Selecionar imagem...</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('ola!')} style={styles.button}>
+          <Text style={styles.buttonLabel}>Compartilhar imagem...</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +27,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 35,
+  },
+  image: {
+    flex: 1,
+  },
+  actions: {
+    flex: 1,
+    paddingTop: 10,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#555',
+    padding: 10,
+    borderRadius: 25,
+    backgroundColor: '#333'
+  },
+  buttonLabel: {
+    color: '#fff'
   },
 });
