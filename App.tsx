cae0e5b0c9d485e74ce29a5a540e12f8247c356b
Emilio Resende <emilio.murta@msitbrasil.com>
2022-09-28 21:56:08 -0300

    add share image

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Dimensions,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import * as ImagePicker from 'expo-image-picker';
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
 
 interface LocalImage {
   defaultUri?: string;
@@ -13,40 +23,71 @@ export default function App() {
   const [selectedImg, setSelectedImg] = useState<LocalImage | null>(null);
 
   useEffect(() => {
-    setSelectedImg({ defaultUri: 'https://reactjs.org/logo-og.png' } as LocalImage)
+    fetchRandomImageAsync();
   }, []);
 
   const openImagePickerAsync = async () => {
     const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!permission.granted) {
-      alert('Permissão não concedida...');
+      alert("Permissão não concedida...");
       return;
     }
 
     const picker = await ImagePicker.launchImageLibraryAsync();
     if (picker.cancelled) return;
 
-    setSelectedImg({remoteUri: picker.uri} as LocalImage);
-  }
+    setSelectedImg({ remoteUri: picker.uri } as LocalImage);
+  };
+
+  const openShareDialogAsync = async () => {
+    if ((await Sharing.isAvailableAsync()) && selectedImg?.remoteUri) {
+      Sharing.shareAsync(selectedImg.remoteUri);
+      return;
+    }
+
+    alert("Imagem não selecionada");
+    return;
+  };
+
+  const fetchRandomImageAsync = async () => {
+    const headers = new Headers();
+    headers.append(
+      "Authorization",
+      "ML753BSmTZzdvu7dJgJ05RPoCGRBWRdddRv1FD9bVjQ"
+    );
+    const parms = {
+      method: "GET",
+      headers,
+    };
+    const response = await fetch(
+      "https://api.unsplash.com/photos/random?client_id=MBHpyxQMkz8yn9nnolr0rWUZ0lm0FX_n47ZgZ8RBX_I",
+      parms
+    );
+    const contextData = await response.json();
+    setSelectedImg({
+      defaultUri: contextData['urls']['thumb'],
+    } as LocalImage)
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.text}>
         <Text>Por favor, selecione uma imagem...</Text>
       </View>
-      
-      <Image 
-        style={styles.image} 
-        source={{uri: selectedImg?.remoteUri ?? selectedImg?.defaultUri}} 
-        resizeMode="contain" 
+
+      <Image
+        style={styles.image}
+        source={{ uri: selectedImg?.remoteUri ?? selectedImg?.defaultUri }}
+        resizeMode="contain"
       />
-      
+
       <View style={styles.actions}>
         <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
           <Text style={styles.buttonLabel}>Selecionar imagem...</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert('ola!')} style={styles.button}>
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
           <Text style={styles.buttonLabel}>Compartilhar imagem...</Text>
+          <Ionicons name="share-outline" size={16} style={styles.buttonLabel} />
         </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
@@ -56,16 +97,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 35,
   },
   image: {
@@ -76,33 +117,36 @@ const styles = StyleSheet.create({
   actions: {
     flex: 1,
     paddingTop: 10,
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
   },
   button: {
+    display: "flex",
+    flexDirection: "row",
     borderWidth: 1,
-    borderColor: '#555',
+    borderColor: "#555",
     padding: 10,
-    maxHeight: 40,
     ...Platform.select({
       ios: {
         borderRadius: 25,
-        backgroundColor: '#333',
+        backgroundColor: "#333",
+        maxHeight: Dimensions.get("screen").height * 0.05,
       },
       android: {
         borderRadius: 5,
-        backgroundColor: '#aaa',
-      }
-    })
+        backgroundColor: "#aaa",
+        maxHeight: Dimensions.get("screen").height * 0.06,
+      },
+    }),
   },
   buttonLabel: {
     ...Platform.select({
       ios: {
-        color: '#fff'
+        color: "#fff",
       },
       android: {
-        color: '#000'
-      }
-    })
+        color: "#000",
+      },
+    }),
   },
 });
